f76aea8600df676b15dc50c538e8e781ed0739b4
Nick Hunter <hunter.497@gmail.com>
2016-03-08 14:00:02 -0500

    Setting up the Basics of the Platformer Tutorial
    Following CodingMadeEasy platformer tutorial for monogame. Taking a
    different approach to passing variables, instead of a statics file.

---
+++ Platformer/Platformer/Managers/ScreenManager.cs
@@ -0,0 +1,16 @@
+﻿using Platformer.Screens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer.Managers
+{
+    public class ScreenManager
+    {
+        Dictionary<string, GameScreen> screens = new Dictionary<string, GameScreen>();
+
+        Stack<GameScreen> screenStack = new Stack<GameScreen>();
+    }
+}
---
+++ Platformer/Platformer/Screens/GameScreen.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer.Screens
+{
+    public class GameScreen
+    {
+        public virtual void Initialize()
+        {
+
+        }
+
+        public virtual void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch){ }
+    }
+}
